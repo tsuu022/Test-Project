8f0318ab9535c6797eb2f4fac83423afdc272866
TsuRuu <tsuurugi22@gmail.com>
2016-04-08 16:55:48 +0300

    Space scene.

---
+++ Assets/Scripts/MapDynamics.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MapDynamics : MonoBehaviour {
+
+	public float minGlossiness = 0.0f;
+	public float maxGlossiness = 0.9f;
+	public float glossinessSpeed = 0.5f;
+
+	public float smallStarsMoveSpeed = 0.25f;
+
+	//MOVE
+	private static float smallStarsMoveCount = 5.0f;
+	private static float smallStarsScaleCount = 200.0f + 5.0f; //200 - def. value
+	private Vector3 smallStarsPos1 = new Vector3(-smallStarsMoveCount, 200.0f, -smallStarsMoveCount);
+	private Vector3 smallStarsPos2 = new Vector3(smallStarsMoveCount, smallStarsScaleCount, smallStarsMoveCount);
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		foreach (Transform child in transform)
+		{
+			if (child.name == "SmallStarsPlane") {
+				child.GetComponent<Renderer> ().material.SetFloat ("_Glossiness", minGlossiness + Mathf.PingPong (Time.time * glossinessSpeed, maxGlossiness));
+				child.transform.position = Vector3.Lerp (smallStarsPos1, smallStarsPos2, Mathf.PingPong(Time.time * smallStarsMoveSpeed,  1.0f));
+			}
+			if (child.name == "BigStarsPlane") {
+				child.GetComponent<Renderer> ().material.SetFloat ("_Glossiness", minGlossiness + Mathf.PingPong (Time.time * glossinessSpeed, maxGlossiness));
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/ShipContorller.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using UnityEngine;
+
+public class ShipContorller : MonoBehaviour {
+
+    public GameObject ship;
+
+    public float moveSpeed = 100.0f;
+    private float speedCoeff = 1.5f;
+
+    public bool isHyper = false;
+    public float hyperSpeed = 200;
+
+    private Vector3 newPosition;
+
+    void Start()
+    {
+        newPosition = transform.position;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit))
+            {
+                newPosition = new Vector3(hit.point.x, transform.position.y, hit.point.z); //y pos disabled cuz we have a 2d game
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+            isHyper = true;
+
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+            isHyper = false;
+
+        //Debug.DrawRay(ship.transform.position, newPosition - transform.position, Color.red);
+
+        if (transform.position != newPosition)
+        {
+            rotateShip(newPosition);
+            moveShip(newPosition);
+        }
+    }
+
+    void rotateShip(Vector3 vector)
+    {
+        ship.transform.rotation = Quaternion.LookRotation(vector - transform.position);
+    }
+
+    void moveShip(Vector3 vector)
+    {
+        float speed;
+        if (isHyper) speed = hyperSpeed; else speed = moveSpeed;
+        transform.position = Vector3.MoveTowards(transform.position, vector, Time.deltaTime * speed * speedCoeff);
+    }
+
+}
